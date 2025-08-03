b709077db86b127aaa2740d45e687ec8b45261e5
KIRUPAKARAN S <kirupakaran.s151205@gmail.com>
2025-08-03 06:58:48 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.4 MB (100.00%)

---
+++ 1329-minimum-cost-to-move-chips-to-the-same-position/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minCostToMoveChips(int[] position) {
+        int cost=0;
+        int odd=0,even=0;
+        for(int i:position){
+            if(i%2==0)  even++;
+            else odd++;
+        }
+        return Math.min(odd,even);
+    }
+}
