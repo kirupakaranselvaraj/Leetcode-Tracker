5940134d081469f1a730732475f37e8d9f845b2f
KIRUPAKARAN S <kirupakaran.s151205@gmail.com>
2025-09-23 08:33:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 23.4 MB (66.00%)

---
+++ 1777-determine-if-two-strings-are-close/solution.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        if (word1.size() != word2.size()) return false;
+
+        vector<int> freq1(26, 0), freq2(26, 0);
+
+        for (char c : word1) freq1[c - 'a']++;
+        for (char c : word2) freq2[c - 'a']++;
+
+        // Step 1: Check if both have same set of characters
+        for (int i = 0; i < 26; i++) {
+            if ((freq1[i] == 0 && freq2[i] != 0) ||
+                (freq1[i] != 0 && freq2[i] == 0)) {
+                return false;
+            }
+        }
+
+        // Step 2: Compare sorted frequency counts
+        sort(freq1.begin(), freq1.end());
+        sort(freq2.begin(), freq2.end());
+
+        return freq1 == freq2;
+    }
+};
+
