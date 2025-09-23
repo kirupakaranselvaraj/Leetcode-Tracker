30a1d4d27eaba018ac29086106ecddeb6741f12a
KIRUPAKARAN S <kirupakaran.s151205@gmail.com>
2025-09-23 02:27:58 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.9 MB (99.95%)

--- 0058-length-of-last-word/solution.java
+++ 0058-length-of-last-word/solution.java
@@ -1,10 +1,7 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int length = 0;
-        int i = s.length() - 1;
-        while (i >= 0 && s.charAt(i) == ' ') {
-            i--;
-        }
+        int length = 0, i = s.length() - 1;
+        while (i >= 0 && s.charAt(i) == ' ') i--;  // skip trailing spaces
         while (i >= 0 && s.charAt(i) != ' ') {
             length++;
             i--;
