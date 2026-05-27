5b8a4c10e19de3c347371ba3463d3b277647ab0e
KIRUPAKARAN S <kirupakaran.s151205@gmail.com>
2026-05-27 09:25:25 +0000

    Sync LeetCode submission Runtime - 4 ms (100.00%), Memory - 45.9 MB (72.05%)

--- 0009-palindrome-number/solution.java
+++ 0009-palindrome-number/solution.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0)) {
+        if (x < 0) {
             return false;
         }
-
-        int revertedNumber = 0;
-        while (x > revertedNumber) {
-            revertedNumber = revertedNumber * 10 + x % 10;
-            x /= 10;
+        int original = x;
+        int reverse = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            reverse = reverse * 10 + digit;
+            x = x / 10;
         }
-
-        return x == revertedNumber || x == revertedNumber / 10;
+        return original == reverse;
     }
 }
