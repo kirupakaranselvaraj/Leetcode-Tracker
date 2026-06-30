b93af68dc494e1f72f0beee054bedc485cf6f930
KIRUPAKARAN S <kirupakaran.s151205@gmail.com>
2026-06-30 10:44:04 +0000

    Sync LeetCode submission Runtime - 3 ms (78.56%), Memory - 42.9 MB (85.81%)

---
+++ 0228-summary-ranges/solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+
+        List<String> list = new ArrayList<>();
+
+        if (nums.length == 0)
+            return list;
+
+        int start = nums[0];
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (i == nums.length - 1 || nums[i] + 1 != nums[i + 1]) {
+
+                if (start == nums[i]) {
+                    list.add(String.valueOf(start));
+                } else {
+                    list.add(start + "->" + nums[i]);
+                }
+
+                if (i != nums.length - 1)
+                    start = nums[i + 1];
+            }
+        }
+
+        return list;
+    }
+}
