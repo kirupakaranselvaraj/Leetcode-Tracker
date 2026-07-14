19045988be5cae50f1822d8201400be436a117f4
KIRUPAKARAN S <kirupakaran.s151205@gmail.com>
2026-07-14 12:22:35 +0000

    Sync LeetCode submission Runtime - 1 ms (99.81%), Memory - 43.4 MB (20.22%)

---
+++ 0389-find-the-difference/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int sumS = 0;
+        int sumT = 0;
+        for (char ch : s.toCharArray()) {
+            sumS += ch;
+        }
+        for (char ch : t.toCharArray()) {
+            sumT += ch;
+        }
+        return (char) (sumT - sumS);
+    }
+}
